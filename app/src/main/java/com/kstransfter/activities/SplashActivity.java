f193410f3067ebc848c4920d60d52e6850456701
rahulkumarit <rk74535@gmail.com>
2018-09-11 23:27:54 +0530

    commit new

--- app/src/main/java/com/kstransfter/activities/SplashActivity.java
+++ app/src/main/java/com/kstransfter/activities/SplashActivity.java
@@ -1,10 +1,15 @@
 package com.kstransfter.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+
 import com.kstransfter.R;
+import com.kstransfter.utils.SessionManager;
 
 public class SplashActivity extends BaseActivity {
+
+    private SessionManager sessionManager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -12,18 +17,29 @@ public class SplashActivity extends BaseActivity {
         setContentView(R.layout.activity_splash);
         try {
             initial();
-          } catch (Exception e) {
-         }
+        } catch (Exception e) {
+        }
     }
 
     @Override
     public void initial() {
+        sessionManager = new SessionManager(this);
         new android.os.Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-
+                if (sessionManager.isLoggedIn()) {
+                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                } else {
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                }
             }
-        },100);
+        }, 200);
 
 
     }

f464a2fafde45607e5ef4a3765f7f3bf06109a70
rahulkumarit <rk74535@gmail.com>
2018-09-12 23:43:47 +0530

    working on signup screen

--- app/src/main/java/com/kstransfter/activities/LoginActivity.java
+++ app/src/main/java/com/kstransfter/activities/LoginActivity.java
@@ -1,10 +1,19 @@
 package com.kstransfter.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.kstransfter.R;
 
 public class LoginActivity extends BaseActivity {
+
+    private TextView txtLogin, txtSignUp;
+    private EditText edtUserName, edtPassword;
 
 
     @Override
@@ -13,14 +22,40 @@ public class LoginActivity extends BaseActivity {
         setContentView(R.layout.activity_login);
         try {
             initial();
-           } catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
     @Override
     public void initial() {
+        txtLogin = findViewById(R.id.txtLogin);
+        txtSignUp = findViewById(R.id.txtSignUp);
+        edtUserName = findViewById(R.id.edtUserName);
+        edtPassword = findViewById(R.id.edtPassword);
 
+        txtLogin.setOnClickListener(v -> {
+            clickLogin();
+        });
+        txtSignUp.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+            startActivity(intent);
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+        });
     }
+
+    private void clickLogin() {
+        String userName = edtUserName.getText().toString().trim();
+        String password = edtPassword.getText().toString().trim();
+        if (TextUtils.isEmpty(userName)) {
+            Toast.makeText(LoginActivity.this, "enter user name", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(password)) {
+            Toast.makeText(LoginActivity.this, "enter password", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+        }
+    }
+
 }
--- app/src/main/java/com/kstransfter/activities/MainActivity.java
+++ app/src/main/java/com/kstransfter/activities/MainActivity.java
@@ -12,7 +12,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
 import com.kstransfter.R;
 
 public class MainActivity extends AppCompatActivity

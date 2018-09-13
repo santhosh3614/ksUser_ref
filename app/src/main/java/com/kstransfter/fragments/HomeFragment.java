d22b084ba6bbc7db77da99ed3e8519011fbbc815
Rahul Kumar <rahulkumar@devrepublic.nl>
2018-09-13 18:37:34 +0530

    working on map integration

--- app/src/main/java/com/kstransfter/fragments/HomeFragment.java
+++ app/src/main/java/com/kstransfter/fragments/HomeFragment.java
@@ -6,9 +6,19 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.kstransfter.R;
 
-public class HomeFragment extends BaseFragment {
+public class HomeFragment extends BaseFragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
 
     @Nullable
     @Override
@@ -20,19 +30,28 @@ public class HomeFragment extends BaseFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
         try {
             initital();
          } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
     @Override
     public void initital() {
 
+
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+     }
 
-}
+
+ }

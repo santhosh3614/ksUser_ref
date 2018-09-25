e59dbccb25bead47c677dbe9b0e7270503f3d3a6
Rahul Kumar <rahulkumar@devrepublic.nl>
2018-09-25 13:52:03 +0530

    change step class

--- app/src/main/java/com/kstransfter/models/Step.java
+++ app/src/main/java/com/kstransfter/models/Step.java
@@ -8,13 +8,13 @@ public class Step {
 
     @SerializedName("distance")
     @Expose
-    private Distance_ distance;
+    private Distance distance;
     @SerializedName("duration")
     @Expose
-    private Duration_ duration;
+    private Duration duration;
     @SerializedName("end_location")
     @Expose
-    private EndLocation_ endLocation;
+    private EndLocation endLocation;
     @SerializedName("html_instructions")
     @Expose
     private String htmlInstructions;
@@ -23,7 +23,7 @@ public class Step {
     private Polyline polyline;
     @SerializedName("start_location")
     @Expose
-    private StartLocation_ startLocation;
+    private StartLocation startLocation;
     @SerializedName("travel_mode")
     @Expose
     private String travelMode;
@@ -31,27 +31,27 @@ public class Step {
     @Expose
     private String maneuver;
 
-    public Distance_ getDistance() {
+    public Distance getDistance() {
         return distance;
     }
 
-    public void setDistance(Distance_ distance) {
+    public void setDistance(Distance distance) {
         this.distance = distance;
     }
 
-    public Duration_ getDuration() {
+    public Duration getDuration() {
         return duration;
     }
 
-    public void setDuration(Duration_ duration) {
+    public void setDuration(Duration duration) {
         this.duration = duration;
     }
 
-    public EndLocation_ getEndLocation() {
+    public EndLocation getEndLocation() {
         return endLocation;
     }
 
-    public void setEndLocation(EndLocation_ endLocation) {
+    public void setEndLocation(EndLocation endLocation) {
         this.endLocation = endLocation;
     }
 
@@ -71,11 +71,11 @@ public class Step {
         this.polyline = polyline;
     }
 
-    public StartLocation_ getStartLocation() {
+    public StartLocation getStartLocation() {
         return startLocation;
     }
 
-    public void setStartLocation(StartLocation_ startLocation) {
+    public void setStartLocation(StartLocation startLocation) {
         this.startLocation = startLocation;
     }
 

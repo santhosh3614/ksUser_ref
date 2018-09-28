4fa331f4c71d3a8d5c5c46df9a2f2b075a900890
Rahul Kumar <rahulkumar@devrepublic.nl>
2018-09-28 18:57:11 +0530

    working on tutorial

--- app/src/main/java/com/kstransfter/activities/TutorialActivity.java
+++ app/src/main/java/com/kstransfter/activities/TutorialActivity.java
@@ -1,15 +1,24 @@
 package com.kstransfter.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.widget.TextView;
 
 import com.kstransfter.R;
 
+import java.util.ArrayList;
+
 public class TutorialActivity extends BaseActivity {
 
     private TextView txtNext;
+    private ArrayList<Fragment> fragments = new ArrayList<>();
+    private ViewPager viewpager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,13 +34,37 @@ public class TutorialActivity extends BaseActivity {
     @Override
     public void initial() {
         txtNext = findViewById(R.id.txtNext);
+        viewpager = findViewById(R.id.viewpager);
         txtNext.setOnClickListener(v -> {
             Intent intent = new Intent(TutorialActivity.this, EnterMobilNumberActivity.class);
             startActivity(intent);
             finish();
             overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
         });
+        ScreenSlidePagerAdapter screenSlidePagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager(), this, fragments);
+        viewpager.setAdapter(screenSlidePagerAdapter);
+      }
+}
 
 
+class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+
+    private Context context;
+    private ArrayList<Fragment> fragments;
+
+    public ScreenSlidePagerAdapter(FragmentManager fm, Context context, ArrayList<Fragment> fragments) {
+        super(fm);
+        this.context = context;
+        this.fragments = fragments;
     }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
 }
---
+++ app/src/main/java/com/kstransfter/fragments/TutorialThree.java
@@ -0,0 +1,31 @@
+package com.kstransfter.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kstransfter.R;
+
+public class TutorialThree extends BaseFragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_tutorial_one, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void initital() {
+
+    }
+
+
+}
